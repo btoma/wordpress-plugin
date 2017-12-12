336fd7a7e5df752771432ba31a27472a56c2bd4d
Biven <biven@lougeek.com>
2017-12-12 09:28:56 -0500

    added css style and get treehouse json profile and save it to wp-options and show the profile on front-end

--- inc/options-page-wrapper.php
+++ inc/options-page-wrapper.php
@@ -1,10 +1,7 @@
-
-<h2><?php esc_attr_e( '2 Columns Layout: static (px)', 'WpAdminStyle' ); ?></h2>
-
 <div class="wrap">
 
       <div id="icon-options-general" class="icon32"></div>
-      <h1><?php esc_attr_e( 'Heading', 'WpAdminStyle' ); ?></h1>
+      <h2>The Official Treehouse Badges Plugin</h2>
 
       <div id="poststuff">
 
@@ -14,74 +11,151 @@
                   <div id="post-body-content">
 
                         <div class="meta-box-sortables ui-sortable">
+                              <?php if (!isset($wptreehouse_username) || $wptreehouse_username == ''): ?>
+                                    <div class="postbox">
 
-                              <div class="postbox">
+                                          <h3><span>Let's Get Started!</span></h3>
+                                          <div class="inside">
 
-                                    <div class="handlediv" title="Click to toggle"><br></div>
-                                    <!-- Toggle -->
+                                                <form name="wptreehouse_username_form" method="post" action="">
+                                                      <input type="hidden" name="wptreehouse_form_submitted" value="Y">
+                                                      <table class="form-table">
+                                                            <tr>
+                                                                  <td>
+                                                                        <label for="wptreehouse_username">Treehouse
+                                                                              username</label>
+                                                                  </td>
+                                                                  <td>
+                                                                        <input name="wptreehouse_username"
+                                                                               id="wptreehouse_username" type="text"
+                                                                               value="" class="regular-text"/>
+                                                                  </td>
+                                                            </tr>
+                                                      </table>
 
-                                    <h2 class="hndle"><span><?php esc_attr_e( 'Main Content Header', 'WpAdminStyle' ); ?></span>
-                                    </h2>
+                                                      <p>
+                                                            <input class="button-primary" type="submit"
+                                                                   name="wptreehouse_usename_submit" value="Save"/>
+                                                      </p>
 
-                                    <div class="inside">
-                                          <h2><?php esc_attr_e( 'Tables', 'WpAdminStyle' ); ?></h2>
-                                          <table class="form-table">
-                                                <tr>
-                                                      <td class="row-title">
-                                                            <label for="wptreehouse_username">wptreehouse_username</label>
-                                                      </td>
-                                                      <td>
-                                                            <input name="wptreehouse_username" id="wptreehouse_username" type="text" value="" class="regular-text" /><br>
-                                                      </td>
-                                                </tr>
-                                          </table>
+                                                </form>
 
+                                          </div> <!-- .inside -->
+
+                                    </div> <!-- .postbox -->
+                              <?php else: ?>
+                                    <div class="postbox">
+
+                                          <h3><span>Most Recent Badges</span></h3>
+                                          <div class="inside">
+
+                                                <p>Below are your 20 most recent badges</p>
+
+                                                <ul class="wptreehouse-badges">
+
+                                                      <?php for ($i = 0; $i < 20; $i++): ?>
+                                                            <li>
+                                                                  <ul>
+                                                                        <li>
+                                                                              <img width="120px" src="<?php echo $wptreehouse_profile->{'badges'}[$i]->{'icon_url'}; ?>">
+                                                                        </li>
+                                                                        <?php if( $wptreehouse_profile->{'badges'}[$i]->{'url'} != $wptreehouse_profile->{'profile_url'}): ?>
+                                                                        <li class="wptreehouse-badge-name">
+                                                                              <a href="<?php echo $wptreehouse_profile->{'badges'}[$i]->{'url'}; ?>">
+                                                                                    <?php echo $wptreehouse_profile->{'badges'}[$i]->{'name'}; ?>
+                                                                              </a>
+                                                                        </li>
+                                                                        <li class="wptreehouse-project-name">
+                                                                              <a href="<?php echo $wptreehouse_profile->{'badges'}[$i]->{'courses'}[1]->{'url'}; ?>">
+                                                                                    <?php echo $wptreehouse_profile->{'badges'}[$i]->{'courses'}[1]->{'title'}; ?>
+                                                                              </a>
+                                                                        </li>
+                                                                        <?php else: ?>
+                                                                        <li class="wptreehouse-badge-name">
+                                                                              <?php echo $wptreehouse_profile->{'badges'}[$i]->{'name'}; ?>
+                                                                        </li>
+                                                                        <?php endif;?>
+                                                                  </ul>
+                                                            </li>
+                                                      <?php endfor; ?>
+
+                                                </ul>
+
+                                          </div> <!-- .inside -->
+
+                                    </div> <!-- .postbox -->
+                                    <?php if ($display_json == true): ?>
+                                    <div class="postbox">
+                                          <h3><span>json feed</span></h3>
+                                          <div class="inside">
+                                                <p>
+                                                      <?php echo $wptreehouse_profile->{'name'}; ?>
+                                                </p>
+                                                <p>
+                                                      <?php echo $wptreehouse_profile->{'profile_url'}; ?>
+                                                </p>
+                                                <p>
+                                                      <?php echo $wptreehouse_profile->{'badges'}[1]->{'courses'}[1]->{'title'}; ?>
+                                                </p>
+                                                <pre><code>
+                                                            <?php var_dump($wptreehouse_profile); ?>
+                                                      </code></pre>
+                                          </div>
                                     </div>
-                                    <!-- .inside -->
+                                    <?php endif;?>
+                              <?php endif; ?>
+                        </div> <!-- .meta-box-sortables .ui-sortable -->
 
-                              </div>
-                              <!-- .postbox -->
-
-                        </div>
-                        <!-- .meta-box-sortables .ui-sortable -->
-
-                  </div>
-                  <!-- post-body-content -->
+                  </div> <!-- post-body-content -->
 
                   <!-- sidebar -->
                   <div id="postbox-container-1" class="postbox-container">
 
                         <div class="meta-box-sortables">
+                              <?php if (isset($wptreehouse_username) && $wptreehouse_username != ''): ?>
+                                    <div class="postbox">
 
-                              <div class="postbox">
+                                          <h3><span><?php echo $wptreehouse_profile->{'name'}; ?>'s Profile</span></h3>
+                                          <div class="inside">
 
-                                    <div class="handlediv" title="Click to toggle"><br></div>
-                                    <!-- Toggle -->
+                                                <p><img width="100%" class="wptreehouse-gravatar"
+                                                        src="<?php echo $wptreehouse_profile->{'gravatar_url'}; ?>"
+                                                        alt="Mike the Frog Gravatar"></p>
 
-                                    <h2 class="hndle"><span><?php esc_attr_e(
-                                                    'Sidebar Content Header', 'WpAdminStyle'
-                                                ); ?></span></h2>
+                                                <ul class="wptreehouse-badges-and-points">
 
-                                    <div class="inside">
-                                          <p><?php esc_attr_e( 'Everything you see here, from the documentation to the code itself, was created by and for the community. WordPress is an Open Source project, which means there are hundreds of people all over the world working on it. (More than most commercial platforms.) It also means you are free to use it for anything from your cat’s home page to a Fortune 500 web site without paying anyone a license fee and a number of other important freedoms.',
-                                                    'WpAdminStyle' ); ?></p>
-                                    </div>
-                                    <!-- .inside -->
+                                                      <li>Badges: <strong><?php echo count($wptreehouse_profile->{'badges'}); ?></strong></li>
+                                                      <li>Points: <strong><?php echo $wptreehouse_profile->{'points'}->{'total'}; ?></strong></li>
 
-                              </div>
-                              <!-- .postbox -->
+                                                </ul>
+                                                <form name="wptreehouse_username_form" method="post" action="">
+                                                      <input type="hidden" name="wptreehouse_form_submitted" value="Y">
+                                                     <p>
+                                                           <label for="wptreehouse_username">Username</label>
+                                                     </p>
+                                                      <p>
+                                                      <input name="wptreehouse_username"
+                                                             id="wptreehouse_username" type="text"
+                                                             value="<?php echo $wptreehouse_username; ?>"/>
+                                                      </p>
+                                                      <p>
+                                                            <input class="button-primary" type="submit"
+                                                                   name="wptreehouse_usename_submit" value="Save"/>
+                                                      </p>
 
-                        </div>
-                        <!-- .meta-box-sortables -->
+                                                </form>
 
-                  </div>
-                  <!-- #postbox-container-1 .postbox-container -->
+                                          </div> <!-- .inside -->
 
-            </div>
-            <!-- #post-body .metabox-holder .columns-2 -->
+                                    </div> <!-- .postbox -->
+                              <?php endif; ?>
+                        </div> <!-- .meta-box-sortables -->
+
+                  </div> <!-- #postbox-container-1 .postbox-container -->
+
+            </div> <!-- #post-body .metabox-holder .columns-2 -->
 
             <br class="clear">
-      </div>
-      <!-- #poststuff -->
+      </div> <!-- #poststuff -->
 
 </div> <!-- .wrap -->
---
+++ wptreehouse-badges.php
@@ -0,0 +1,99 @@
+<?php
+
+/*
+ *	Plugin Name: Official Treehouse Badges Plugin
+ *	Plugin URI: http://wptreehouse.com/wptreehouse-badges-plugin/
+ *	Description: Provides both widgets and shortcodes to help you display your Treehouse profile badges on your website.  The official Treehouse badges plugin.
+ *	Version: 1.0
+ *	Author: Biven Toma
+ *	Author URI:
+ *	License: GPL2
+ *
+*/
+
+/*
+ *	Assign global variables
+ *
+*/
+
+$plugin_url = WP_PLUGIN_URL . '/wptreehouse-badges';
+$options = array();
+$display_json = false;
+/*
+ *	Add a link to our plugin in the admin menu
+ *	under 'Settings > Treehouse Badges'
+ *
+*/
+
+function wptreehouse_badges_menu() {
+
+	/*
+	 * 	Use the add_options_page function
+	 * 	add_options_page( $page_title, $menu_title, $capability, $menu-slug, $function ) 
+	 *
+	*/
+
+	add_options_page(
+		'Official Treehouse Badges Plugin',
+		'Treehouse Badges',
+		'manage_options',
+		'wptreehouse-badges',
+		'wptreehouse_badges_options_page'
+	);
+
+}
+add_action( 'admin_menu', 'wptreehouse_badges_menu' );
+
+
+function wptreehouse_badges_options_page() {
+
+	if( !current_user_can( 'manage_options' ) ) {
+
+		wp_die( 'You do not have suggicient permissions to access this page.' );
+
+	}
+
+      global $plugin_url;
+      global $options;
+      global $display_json;
+
+      if(isset($_POST['wptreehouse_form_submitted'])) {
+            $hidden_filed = esc_html($_POST['wptreehouse_form_submitted']);
+            if( $hidden_filed == 'Y'){
+                  $wptreehouse_username             = esc_html($_POST['wptreehouse_username']);
+                  $wptreehouse_profile              = wptreehouse_badges_get_profile($wptreehouse_username);
+                  $options['wptreehouse_username']  = $wptreehouse_username;
+                  $options['wptreehouse_profile']   = $wptreehouse_profile;
+                  $options['last_update']           = time();
+
+                  update_option('wptreehouse_badges', $options);
+            }
+      }
+      $options = get_option('wptreehouse_badges');
+      if($options != '')
+      {
+            $wptreehouse_username = $options['wptreehouse_username'];
+            $wptreehouse_profile = $options['wptreehouse_profile'];
+      }
+
+	require( 'inc/options-page-wrapper.php' );
+
+}
+
+function wptreehouse_badges_get_profile($wptreehouse_username) {
+      $json_feed_url = 'https://teamtreehouse.com/' . $wptreehouse_username . '.json';
+      $args = array('timeout' => 120);
+
+      $json_feed = wp_remote_get($json_feed_url, $args);
+      $wptreehouse_profile = json_decode($json_feed['body']);
+      return $wptreehouse_profile;
+
+}
+function wptreehouse_badges_styles() {
+      wp_enqueue_style('wptreehouse_badges_styles',plugins_url('wptreehouse-badges/wptreehouse-badges.css'));
+}
+
+add_action('admin_head', 'wptreehouse_badges_styles');
+
+
+?>
